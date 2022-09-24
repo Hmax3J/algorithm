b8803c5bfbde25d82a5caddef3b1f73b1666caec
'임제훈' <'Hmax3J@gmail.com'>
2022-09-24 21:52:01 +0900

    2022.09.24

---
+++ baekjoon/src/level1/Remainder.java
@@ -0,0 +1,22 @@
+package level1;
+
+import java.util.Scanner;
+
+public class Remainder {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String ABC = sc.nextLine();
+		
+		String[] data = ABC.split(" "); 
+		
+		int A = Integer.parseInt(data[0]); 
+		int B = Integer.parseInt(data[1]);
+		int C = Integer.parseInt(data[2]);
+		
+		System.out.println((A + B) % C);
+		System.out.println(((A%C) + (B%C)) % C);
+		System.out.println((A * B) % C);
+		System.out.println(((A % C) * (B % C)) % C);
+	}
+}
