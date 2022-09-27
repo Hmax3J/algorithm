4955f0f28b93cbd1214071df12178fdd2192d681
Hmax3J <Hmax3J@gmail.com>
2022-09-27 14:46:40 +0900

    2022.09.27

--- baekjoon/src/level2/Grade.java
+++ baekjoon/src/level2/Grade.java
@@ -8,11 +8,20 @@ public class Grade {
 		
 		int score = sc.nextInt();
 		
-		String result = score >= 90 ? "A" : score >= 80 ? "B" : 
-						score >= 70 ? "C" : score >= 60 ? "D" : "F";
+		String grade = "";
 		
-		if(0 <= score && score <= 100) {
-			System.out.println(result);
-		} else System.out.println("0점 이상 100점 이하의 점수를 입력하세요.");
+		if(!(0 <= score && score <= 100)) {
+			System.out.println("0점 이상 100점 이하의 점수를 입력하세요.");
+		} else {
+			switch(score / 10) {
+				case 9: case 10: grade = "A"; break;
+				case 8: grade = "B"; break;
+				case 7: grade = "C"; break;
+				case 6: grade = "D"; break;
+				default: grade = "F";
+			}
+		}
+		
+		System.out.println(grade);
 	}
 }
