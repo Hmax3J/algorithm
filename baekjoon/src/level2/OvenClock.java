ee0f89eb763db26d4f3a6400ccd88941d853f7df
Hmax3J <Hmax3J@gmail.com>
2022-09-30 14:30:34 +0900

    2022.09.30

---
+++ baekjoon/src/level2/OvenClock.java
@@ -0,0 +1,49 @@
+package level2;
+
+import java.util.Scanner;
+
+public class OvenClock {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String hourMinute = sc.nextLine().trim();
+		
+		String[] data = hourMinute.split(" "); 
+		
+		int a = Integer.parseInt(data[0]); 
+		int b = Integer.parseInt(data[1]);	
+		int c = sc.nextInt();
+		
+		int min = 60 * a + b;   // 시 -> 분
+        min += c;
+ 
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+ 
+        System.out.println(hour + " " + minute);
+		
+		/* 내가 짠 코드.....
+		int m1 = ((b + c) % 60);
+		int m2 = ((b + c) / 60);
+		int h = (a + m2);
+		
+		if((b + c) < 60) {
+			System.out.println(a + " " + (b + c));
+		} else {
+			if(h >= 24) {
+				if(m1 >= 60) {
+					System.out.println(((h - 24) + m2) + " " + (m1 - 60));
+				} else {
+					System.out.println((h - 24) + " " + m1);
+				}
+			} else {
+				if(m1 >= 60) {
+					System.out.println((h + m2) + " " + (m1 - 60));
+				} else {
+					System.out.println(h + " " + m1);
+				}
+			}
+		}
+		*/
+	}
+}
