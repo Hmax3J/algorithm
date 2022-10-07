4a8dddde069fb847c5712063713e934553efacfb
'임제훈' <'Hmax3J@gmail.com'>
2022-10-08 00:18:20 +0900

    2022.10.7

--- baekjoon/src/level4/MaxMin.java
+++ baekjoon/src/level4/MaxMin.java
@@ -8,18 +8,36 @@ public class MaxMin {
 		Scanner sc = new Scanner(System.in);
 		StringBuilder sb = new StringBuilder();
 		
-		int n = sc.nextInt();
+		int n = sc.nextInt(); sc.nextLine();
 		
 		if(!(1 <= n && n <= 1000000)) {
 			System.out.println("범위에 맞는 값을 입력하세요.");
 		} else {
 			String ab = sc.nextLine().trim();
 			StringTokenizer abData = new StringTokenizer(ab);
-			
-			int a = Integer.parseInt(abData.nextToken());
-			int b = Integer.parseInt(abData.nextToken());
-			
-			
+			int max = 0;
+			int min = 0;
+				
+			if(!(n == abData.countTokens())) {	
+				System.out.println("n의 개수에 맞게 입력하세요.");
+			} else {
+				int b = Integer.parseInt(abData.nextToken()); // 처음 값을 초기화 한다.
+				max = b; // 기준점이 될 값을 초기화 한다.
+				min = b;
+				
+				for(int j = 0; j < n - 1; j++) { // 처음 값 초기화 할 때 nextToken을 해서 토큰 개수는 n - 1 이다.
+					int a = Integer.parseInt(abData.nextToken()); // 다음 값을 받는다.
+					
+					if(max < a) { // 초기 값과 다음 값을 비교한다.
+						max = a; 
+					}
+					
+					if(min > a) {
+						min = a;
+					}
+				}
+				System.out.println(min + " " + max);
+			}
 		}
 	}
 }
