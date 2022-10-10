053541bd6e88a1d91aaa221e19e1a98a853ad6ed
'임제훈' <'Hmax3J@gmail.com'>
2022-10-11 00:12:21 +0900

    2022.10.11

--- baekjoon/src/level4/Remainder.java
+++ baekjoon/src/level4/Remainder.java
@@ -1,24 +1,17 @@
 package level4;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Remainder {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] result = new int[10];
-		int count = 0;
+		HashSet<Integer> remainder = new HashSet<Integer>();
 		
 		for(int i = 0; i < 10; i++) {
-			int a = sc.nextInt(); sc.nextLine();
-			
-			if(!(0 <= a && a <= 1000)) {
-				System.out.println("올바른 값을 입력하세요.");
-			} else {
-				result[i] = a % 42;
-				
-				
-				System.out.println(count);
-			}
+			remainder.add(sc.nextInt() % 42);
 		}
+		
+		System.out.println(remainder.size());
 	}
 }
