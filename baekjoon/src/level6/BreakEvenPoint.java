46708a9711147f657b113b16cee0226b8eca068e
'임제훈' <'Hmax3J@gmail.com'>
2022-10-17 00:45:54 +0900

    2022.10.17

--- baekjoon/src/level6/BreakEvenPoint.java
+++ baekjoon/src/level6/BreakEvenPoint.java
@@ -1,7 +1,24 @@
 package level6;
 
+import java.util.Scanner;
+
 public class BreakEvenPoint {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		String scores = sc.nextLine().trim();
+		String[] bep = scores.split(" ");
+		
+		int a = Integer.parseInt(bep[0]);
+		int b = Integer.parseInt(bep[1]);
+		int c = Integer.parseInt(bep[2]);
+		
+		// n * c > a + (n * b) , n = a / c - b
+		if (c <= b) {
+			System.out.println("-1");
+		} 
+		else {
+			System.out.println((a / (c - b)) + 1); // n * c 보다 커야 하니 값이 같아 질 때 + 1 을 한다.
+		}
 	}
 }
