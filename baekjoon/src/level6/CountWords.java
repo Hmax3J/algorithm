a80f2870bf1dc858decbae1f0fb4f088f0238918
'임제훈' <'Hmax3J@gmail.com'>
2022-10-23 00:09:44 +0900

    2022.10.23

--- baekjoon/src/level6/CountWords.java
+++ baekjoon/src/level6/CountWords.java
@@ -1,11 +1,20 @@
 package level6;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class CountWords {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
+		String s = sc.nextLine().trim();
 		
+		if(!(0 <= s.length() && s.length() <= 1000000)) {
+			System.out.println("범위에 맞는 값을 입력하세요.");
+		} else {
+			StringTokenizer sData = new StringTokenizer(s);
+			
+			System.out.println(sData.countTokens());
+		}
 	}
 }
