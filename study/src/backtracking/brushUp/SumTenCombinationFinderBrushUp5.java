820a090309e99976cb43246d40f16e0063e6d37d
임제훈 <Hmax3J@gmail.com>
2024-04-08 04:12:12 +0900

    2024. 4. 8. backtracking
    백트래킹 풀기 및 폴더구조 변경

---
+++ study/src/backtracking/brushUp/SumTenCombinationFinderBrushUp5.java
@@ -0,0 +1,31 @@
+package backtracking.brushUp;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class SumTenCombinationFinderBrushUp5 {
+    private static ArrayList<ArrayList<Integer>> result;
+    private static int n;
+
+    private static void backtrack(int sum, ArrayList<Integer> selectedNums, int start) {
+        if (sum == 10) {
+            result.add(selectedNums);
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (sum + i <= 10) {
+                ArrayList<Integer> list = new ArrayList<>(selectedNums);
+                list.add(i);
+                backtrack(sum + i, list, i + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        n = 5;
+        result = new ArrayList<>();
+        backtrack(0, new ArrayList<>(), 1);
+        System.out.println(result);
+    }
+}
---
+++ study/src/graph/dfs/brushUp/SplittingThePowerGridBrushUp9.java
@@ -0,0 +1,44 @@
+package graph.dfs;
+
+import java.util.ArrayList;
+
+public class SplittingThePowerGridBrushUp9 {
+    private static boolean[] visited;
+    private static ArrayList<Integer>[] adjList;
+    private static int N, answer;
+
+    public static void main(String[] args) {
+        int[][] wires = {{1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {4, 7}, {7, 8}, {7, 9}};
+        int n = 9;
+        N = n;
+        answer = n - 1;
+        visited = new boolean[n + 1];
+
+        adjList = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        for (int[] wire : wires) {
+            adjList[wire[0]].add(wire[1]);
+            adjList[wire[1]].add(wire[0]);
+        }
+
+        dfs(1);
+        System.out.println(answer);
+    }
+
+    private static int dfs(int now) {
+        visited[now] = true;
+        int sum = 0;
+
+        for (int next : adjList[now]) {
+            if (!visited[next]) {
+                int cnt = dfs(next);
+                answer = Math.min(answer, Math.abs(N - cnt * 2));
+                sum += cnt;
+            }
+        }
+        return sum + 1;
+    }
+}
