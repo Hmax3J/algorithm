211a846de1ade6874d51c177b01ba78eb5c76155
임제훈 <Hmax3J@gmail.com>
2024-04-09 04:37:46 +0900

    2024. 4. 9. backtracking

---
+++ study/src/backtracking/brushUp/SumTenCombinationFinderBrushUp25.java
@@ -0,0 +1,30 @@
+package backtracking.brushUp;
+
+import java.util.ArrayList;
+
+public class SumTenCombinationFinderBrushUp25 {
+    private static ArrayList<ArrayList<Integer>> result;
+    private static int n;
+
+    private static void backtrack(int sum, ArrayList<Integer> selectedNums, int start) {
+        if (sum == 10) {
+            result.add(selectedNums);
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            if (sum + i <= 10) {
+                ArrayList<Integer> list = new ArrayList<>(selectedNums);
+                list.add(i);
+                backtrack(sum + i, list, i + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        n = 5;
+        result = new ArrayList<>();
+        backtrack(0, new ArrayList<>(), 1);
+        System.out.println(result);
+    }
+}
