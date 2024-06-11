06fbd250dacbd51d25451bcac63a20291b47afa3
임제훈 <Hmax3J@gmail.com>
2024-06-12 02:42:19 +0900

    2024. 6. 12. 릿코드
    릿코드

---
+++ leetcode/src/grind75/BalancedBinaryTree.java
@@ -0,0 +1,31 @@
+package grind75;
+
+public class BalancedBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int depth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+
+        int left = depth(root.left);
+        int right = depth(root.right);
+
+        return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+}
