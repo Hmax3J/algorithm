edad1d3ddca881d533e8e1a02d05cf7ffa0f1a35
임제훈 <Hmax3J@gmail.com>
2024-06-11 16:49:18 +0900

    2024. 6. 11. 릿코드
    릿코드

---
+++ leetcode/src/grind75/LowestCommonAncestor.java
@@ -0,0 +1,22 @@
+package grind75;
+
+public class LowestCommonAncestor {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (true) {
+            if (root.val > p.val && root.val > q.val)
+                root = root.left;
+            else if (root.val < p.val && root.val < q.val)
+                root = root.right;
+            else
+                return root;
+        }
+    }
+}
