d435194106bf21660d063c6ef17baf4c997259c7
임제훈 <Hmax3J@gmail.com>
2024-06-13 23:36:02 +0900

    2024. 6. 13. 릿코드
    릿코드

---
+++ leetcode/src/grind75/LinkedListCycle.java
@@ -0,0 +1,24 @@
+package grind75;
+
+public class LinkedListCycle {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow_pointer = head, fast_pointer = head;
+        while (fast_pointer != null && fast_pointer.next != null) {
+            slow_pointer = slow_pointer.next;
+            fast_pointer = fast_pointer.next.next;
+            if (slow_pointer == fast_pointer) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
