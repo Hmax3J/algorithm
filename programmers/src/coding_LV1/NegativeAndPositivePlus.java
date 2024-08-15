227cc1786b7bbc098f972674a9d27e169126f3ed
임제훈 <Hmax3J@gmail.com>
2024-08-16 04:48:15 +0900

    2024. 8. 16. 프로그래머스
    프로그래머스

---
+++ programmers/src/coding_LV1/NegativeAndPositivePlus.java
@@ -0,0 +1,22 @@
+package coding_LV1;
+
+public class NegativeAndPositivePlus {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+
+        for (int i = 0; i < signs.length; i++) {
+            if (signs[i]) {
+                answer += absolutes[i];
+            } else {
+                answer -= absolutes[i];
+            }
+        }
+
+        return answer;
+    }
+    /*
+    for (int i = 0; i < signs.length; i++) {
+        answer += absolutes[i] * (signs[i] ? 1 : -1);
+     }
+     */
+}
