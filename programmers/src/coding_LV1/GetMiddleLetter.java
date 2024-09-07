ea85341404564f885bf7e6847a4ddcceab8faacc
임제훈 <Hmax3J@gmail.com>
2024-09-08 00:48:40 +0900

    2024. 9. 8. 프로그래머스
    2024. 9. 8. 프로그래머스

---
+++ programmers/src/coding_LV1/GetMiddleLetter.java
@@ -0,0 +1,19 @@
+package coding_LV1;
+
+public class GetMiddleLetter {
+    public String solution(String s) {
+        String answer = "";
+
+        if (s.length() % 2 == 0) {
+
+            answer = s.substring(s.length() / 2 - 1, s.length() / 2 + 1);
+
+        } else {
+
+            answer = s.substring(s.length() / 2, s.length() / 2 + 1);
+
+        }
+
+        return answer;
+    }
+}
