a94b6e6cdf8a43521279aa47836d9e5937a0a35e
JeHun <62044367+Hmax3J@users.noreply.github.com>
2024-09-22 11:40:09 +0900

    2024. 9. 22. 프로그래머스
    프로그래머스

---
+++ programmers/src/coding_LV2/SeesawPartner.java
@@ -0,0 +1,27 @@
+package coding_LV2;
+
+import java.util.*;
+
+public class SeesawPartner {
+    public long solution(int[] weights) {
+        long answer = 0;
+
+        Map<Double, Integer> map = new HashMap<>();
+
+        double[] divide = new double[]{
+                1.0, 2.0/3, 1.0/2, 3.0/4
+        };
+
+        Arrays.sort(weights);
+        for (int weight : weights) {
+            for (double d : divide) {
+                if (map.containsKey(weight * d)) {
+                    answer += map.get(weight * d);
+                }
+            }
+            map.put((double)weight, map.getOrDefault((double)weight, 0) + 1);
+        }
+
+        return answer;
+    }
+}
