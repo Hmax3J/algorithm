72c58925eb29eb46058e02c52a994ebfa46cf171
임제훈 <Hmax3J@gmail.com>
2024-09-30 04:57:34 +0900

    2024. 9. 30. 프로그래머스
    프로그래머스

--- programmers/src/coding_LV2/Dot.java
+++ programmers/src/coding_LV2/Dot.java
@@ -1,4 +1,17 @@
 package coding_LV2;
 
 public class Dot {
+    public long solution(int k, int d) {
+        long answer = 0;
+
+        for (int a = 0; a * k <= d; a++) {
+            long x = (long) a * k;
+
+            long maxY = (long) Math.sqrt((long) d * d - x * x);
+
+            answer += maxY / k + 1;
+        }
+
+        return answer;
+    }
 }
