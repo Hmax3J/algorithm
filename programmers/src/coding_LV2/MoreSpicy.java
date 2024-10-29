21c7c1bdc2a510bac6144727c8a61c61e0eb84da
임제훈 <Hmax3J@gmail.com>
2024-10-30 00:22:49 +0900

    2024. 10. 30. 프로그래머스
    프로그래머스

---
+++ programmers/src/coding_LV2/MoreSpicy.java
@@ -0,0 +1,30 @@
+package coding_LV2;
+
+import java.util.*;
+
+public class MoreSpicy {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+
+        for (int s : scoville) {
+            heap.add(s);
+        }
+
+        int mixCount = 0;
+
+        while (heap.size() > 1 && heap.peek() < K) {
+            int leastSpicy = heap.poll();
+            int secondLeastSpicy = heap.poll();
+            int newScoville = leastSpicy + (secondLeastSpicy * 2);
+
+            heap.add(newScoville);
+            mixCount++;
+        }
+
+        if (heap.peek() < K) {
+            return -1;
+        }
+
+        return mixCount;
+    }
+}
